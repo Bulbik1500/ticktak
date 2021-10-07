571f55f296e155c671619df1ca6e1182577825a9
Bulbik1500 <Bulbik1500@gmail.com>
2021-10-07 19:20:14 +0000

    niie działa XD

--- src/index.js
+++ src/index.js
@@ -1,30 +1,111 @@
-import React from 'react';
-import ReatDOM, { render } from 'react-dom';
+Skip to content
+panilo
+/
+new_react_tiptaptoe
+Public
+Code
+Issues
+Pull requests
+Actions
+More
+new_react_tiptaptoe/src/index.js /
+@panilo
+panilo Init
+ History
+ 1 contributor
+95 lines (82 sloc)  2.02 KB
+import React from "react";
+import ReactDOM from "react-dom";
 
 class Square extends React.Component {
-    constructor(props){
-        super(props);
-    }
-    render(){
-        return(
-            <button onClick={() => this.props.handleSquareClick()}>
-                {this.props.value}
-            </button>
-        );
-    }
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <button onClick={() => this.props.handleSquareClick()}>
+        {this.props.value}
+      </button>
+    );
+  }
 }
 
-class Board extends React.Component{
-    constructor(props){
-        super(props);
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const rowStartIndexes = [0, 3, 6];
+    const squares = Array(3).fill(null); // [null, null, ...]
+
+    return rowStartIndexes.map((rowStartIndex, arrIndex) => {
+      return (
+        <div className="board-row">
+          {squares.map((square, placeHolderIndex) => {
+            let squareIndex = rowStartIndex + placeHolderIndex;
+            return (
+              <Square
+                value={this.props.squares[squareIndex]}
+                handleSquareClick={() => {
+                  this.props.handleSquareClick(squareIndex);
+                }}
+              />
+            );
+          })}
+        </div>
+      );
+    });
+  }
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      nextSymbol: "X",
+    };
+    this.handleSquareClick = this.handleSquareClick.bind(this);
+  }
+
+  handleSquareClick(index) {
+    console.log(`User clicked ${index}`);
+    const squares = this.state.squares;
+
+    if (squares[index] !== null) {
+      return;
     }
 
-    render(){
-        const rowStartIndexes = [0, 3, 6];
-        const squares = Array(3).fill(null);
-    
-        return rowStartIndexes.map((rowStartIndex, arrIndex) => {
-            let squareIndex = rowStartIndexes + placeHolderIndex;
-        });
-    }
+    squares[index] = this.state.nextSymbol;
+
+    this.setState({
+      squares: squares,
+      nextSymbol: this.state.nextSymbol === "X" ? "O" : "X",
+    });
+
+    /*
+      if(this.state.nextSymbol === "X") {
+        this.setState({nextSymbol: "O"});
+      } else {
+        this.setState({nextSymbol: "X"});
+      }
+    */
+  }
+
+  render() {
+    return (
+      <div className="game">
+        <div className="board">
+          <Board
+            handleSquareClick={this.handleSquareClick}
+            squares={this.state.squares}
+          />
+        </div>
+      </div>
+    );
+  }
 }
+
+ReactDOM.render(<Game />, document.getElementById("root"));
