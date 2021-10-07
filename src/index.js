33d7baf2740b6f72b839791bb939b06c202a2cee
Bulbik1500 <Bulbik1500@gmail.com>
2021-10-07 12:56:07 +0000

    tiptaptoe

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
 import React from 'react';
-import ReatDOM from 'react-dom';
+import ReatDOM, { render } from 'react-dom';
 
-c
+class Square extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <button onClick={() => this.props.handleSquareClick()}>
+                {this.props.value}
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        const rowStartIndexes = [0, 3, 6];
+        const squares = Array(3).fill(null);
+    
+        return rowStartIndexes.map((rowStartIndex, arrIndex) => {
+            let squareIndex = rowStartIndexes + placeHolderIndex;
+        });
+    }
+}
